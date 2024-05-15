3bedc7506b8849e51fa9a92c8d514f61de4816d0
Kushal-Chandar <kushalchandariit@gmail.com>
2024-05-15 07:52:25 +0530

    feat: implemented draw function

--- src/app.ts
+++ src/app.ts
@@ -9,8 +9,13 @@ import {
   Vector3,
   HemisphericLight,
   MeshBuilder,
+  Color3,
+  StandardMaterial,
+  Mesh,
+  EdgesRenderer,
 } from "@babylonjs/core";
 import { AdvancedDynamicTexture, Control, Button } from "@babylonjs/gui";
+import earcut from "earcut";
 
 enum State {
   DRAW = 0,
@@ -66,9 +71,9 @@ class App {
     const gui = AdvancedDynamicTexture.CreateFullscreenUI("UI");
     gui.addControl(this._createUIButton("draw", "Draw", State.DRAW, "-25%"));
     gui.addControl(
-      this._createUIButton("extrude", "Extrude", State.EXTRUDE, "-10%")
+      this._createUIButton("extrude", "Extrude", State.EXTRUDE, "-8.3%")
     );
-    gui.addControl(this._createUIButton("move", "Move", State.MOVE, "10%"));
+    gui.addControl(this._createUIButton("move", "Move", State.MOVE, "8.3%"));
     gui.addControl(this._createUIButton("edit", "Edit", State.EDIT, "25%"));
     return gui;
   }
@@ -84,6 +89,7 @@ class App {
     // gui
     this._gui = this._setupGUI();
 
+    // camera
     var camera = new ArcRotateCamera(
       "Camera",
       Math.PI / 2,
@@ -94,23 +100,93 @@ class App {
     );
     camera.setTarget(Vector3.Zero());
     camera.attachControl(this._canvas, true, true);
+
+    // light
     var light1 = new HemisphericLight(
       "light1",
-      new Vector3(0, 1, 0),
+      new Vector3(1, 1, 1),
       this._scene
     );
     light1.intensity = 0.7;
-    var sphere = MeshBuilder.CreateSphere(
-      "sphere",
-      { diameter: 1 },
-      this._scene
-    );
-    sphere.position.y = 1;
+
+    // ground
     const ground = MeshBuilder.CreateGround(
       "ground",
       { width: 6, height: 6 },
       this._scene
     );
+
+    // Create meterials
+    const pointMaterial = new StandardMaterial("pointMaterial", this._scene);
+    pointMaterial.diffuseColor = new Color3(1, 0, 0); // Red color
+    const lineMaterial = new StandardMaterial("lineMaterial", this._scene);
+    lineMaterial.diffuseColor = new Color3(0, 0, 1); // Blue color
+    const shapeMaterial = new StandardMaterial("shapeMaterial", this._scene);
+    shapeMaterial.diffuseColor = new Color3(0, 1, 0); // Green color
+
+    var shape = [[], []]; // index 0 is points, index 1 is lines
+    var lastMesh = null; // store last mesh drawn to select for extrusion process
+
+    this._canvas.addEventListener("pointerdown", (evt) => {
+      switch (this._state) {
+        case State.DRAW:
+          if (evt.button == 0) {
+            // left click
+            const pickInfo = this._scene.pick(evt.clientX, evt.clientY);
+            if (pickInfo.hit && pickInfo.pickedMesh === ground) {
+              const point = MeshBuilder.CreateSphere(
+                "point",
+                { segments: 16, diameter: 0.1 },
+                this._scene
+              );
+              point.material = pointMaterial;
+              point.position = pickInfo.pickedPoint;
+
+              shape[0].push(point);
+              if (shape[0].length > 1) {
+                const prevPoint: Mesh = shape[0][shape[0].length - 2];
+                const line = MeshBuilder.CreateLines(
+                  "line" + prevPoint.position + point.position,
+                  { points: [prevPoint.position, point.position] },
+                  this._scene
+                );
+                line.material = lineMaterial;
+                shape[1].push(line);
+              }
+            }
+          }
+          if (evt.button == 2) {
+            if (shape[0].length > 2) {
+              const points = shape[0].map((point: Mesh) => point.position);
+              [].concat(...shape).forEach((element: Mesh) => {
+                element.dispose();
+              });
+              var polygon = MeshBuilder.CreatePolygon(
+                "shape" + points[0],
+                {
+                  shape: points,
+                  updatable: true,
+                  depth: 0,
+                  sideOrientation: Mesh.DOUBLESIDE,
+                },
+                this._scene,
+                earcut
+              );
+              polygon.enableEdgesRendering();
+              polygon.edgesWidth = 4;
+              polygon.edgesColor = lineMaterial.diffuseColor.toColor4();
+              lastMesh = polygon;
+              shape = [[], []];
+            }
+          }
+          break;
+        case State.EXTRUDE:
+          break;
+
+        default:
+          break;
+      }
+    });
 
     // hide/show the Inspector
     window.addEventListener("keydown", (ev) => {
